42a3d7de962d3755918130bdd93a2171a3682f91
SpaciousCoder78 <88923986+SpaciousCoder78@users.noreply.github.com>
2022-08-02 22:25:11 +0530

    Update password_manager.py

--- main/password_manager.py
+++ main/password_manager.py
@@ -1,32 +1,81 @@
-#password_manager
-import random
+import mysql
+import mysql.connector
 
-def password_generator():
-    maxlength=12
+conn=mysql.connector.connect(host='localhost',
+                            user="root",
+                            password="123rootsql")
+                            
+if conn.is_connected():
+    print("""--------------------------------Password Manager----------------------------------
+    
+    """)
+    
+cursor=conn.connect()
+cur=conn.cursor()
 
+
+def install():
+    cur.execute("CREATE DATABASE passwordmng;")
+    cur.execute("use passwordmng;")
+    cur.execute("CREATE TABLE passwordmanager(name varchar(15),website varchar(30),password varchar(30));")
+    conn.commit()
+    print("Application setup successfully finished")
+
+def gen():
+    import random
     num=['1','2','3','4','5','6','7','8','9','0']
     letter=["a","b",'c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
     letter_cap=['A','B','C','D','E','F','G','H','I','J','K','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
     symbol=['!','@','#','$','%','^',"&",'*','?']
+    password = ''
+    for i in range(0,12):
+        f = random.randint(1,4)
+        if f==1:
+            password = password + num[random.randint(0,9)]
+        elif f==2:
+            password = password + letter[random.randint(0,25)]
+        elif f==3:
+            password = password + letter_cap[random.randint(0,25)]
+        else:
+            password = password + symbol[random.randint(0,8)]
+    print("generated password -  ",password)
+    name = input("enter your name ")
+    website = input("enter the website corressponding to the password")
+    cur.execute("INSERT INTO passwordmanager(name,website,password) VALUES('{}','{}','{}');".format(name,website,password))
+    conn.commit()
+    print("Entry successfully registered")
 
-    mixture=num+letter+letter_cap+symbol
+def find():
+    name = input("enter your name ")
+    website = input("enter the website's name")
+    cur.execute("SELECT * FROM passwordmanager WHERE name = '{}' AND website = '{}';".format(name,website))
+    print("Here is the entry registered pertaining to the above details")
+    print(cur.fetchone())
 
-    for x in range(maxlength-4):
-        print(random.choice(mixture),end="")
+def findall():
+    name = input("enter your name")
+    cur.execute("SELECT * FROM passwordmanager2 WHERE name = '{}';").format(name)
+    print("Here are all the entries pertaining to the name -  ",name)
+    print(cur.fetchall())
+     
+c =1     
+while c in[1,4] :
+   print("Menu")
+   print("1.Install the application")
+   print("2.generate a password")
+   print("3.retrieve a password")
+   print("4.retrieve all the passwords pertaining to a specific user")
+   print("5.exit the application")
 
-menyoo=1
-while menyoo==1:
-    print("--------------------------------------Password Manager---------------------------------------")
-    print("---------------------------------------version 1.0-------------------------------------------")
-    print("""
-        1.Password Generator
-        2.Add a password
-        3.View saved passwords
-    """)
-    choice=int(input("Enter your choice: "))
-    if choice==1:
-        password_generator()
-    if choice==2:
-        print
-    if choice==3:
-        print
+
+   mento= int(input("enter your choice"))
+   if mento==1:
+        install()
+   elif mento==2:
+        gen()
+   elif mento==3:
+        find()
+   elif mento==4:
+        findall()
+   elif mento==5:
+        break
